18eb1dc7fd3a9d7f9e98bfabb7343ac76a25c9ef
Md Kamal Hossain Chowdhury <kamal.cou@gmail.com>
2026-06-23 16:44:52 -0500

    Add shebang to load_module.sh

---
+++ load_module.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+module load Python
+module load OpenMPI
+module load Rust
+module load cargo-c
+module load Apptainer
+module load git
+module load netCDF
+module load HDF5
+module load SQLite
